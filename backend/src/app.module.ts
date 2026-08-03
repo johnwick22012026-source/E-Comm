cddcf67f6c94d36fa7b5c801814c7c547f5ac089
johnwick22012026-source <johnwick22012026@gmail.com>
2026-08-03 16:33:34 +0530

    feat(ECOMM-27): Implement authentication APIs for registration, verification, login, and logout — backend/src/app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { NotificationModule } from './notifications/notification.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.development'],
+    }),
+    PrismaModule,
+    NotificationModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
