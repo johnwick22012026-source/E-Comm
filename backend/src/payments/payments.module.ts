d7ecd82257ff19f46b301ee14494a6fddb6b90be
johnwick22012026-source <johnwick22012026@gmail.com>
2026-08-04 12:25:53 +0530

    feat(ECOMM-67): Implement backend APIs to save and manage tokenized payment methods — backend/src/payments/payments.module.ts

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -4,16 +4,20 @@ import { PaymentsService } from './payments.service'
 import { PrismaModule } from '../prisma/prisma.module'
 import { PAYMENT_PROVIDER_TOKEN } from './constants'
 import { DummyPaymentProvider } from './strategies/dummy-payment.provider'
+import { SavedPaymentPreferenceController } from './saved-payment-preference/saved-payment-preference.controller'
+import { SavedPaymentPreferenceService } from './saved-payment-preference/saved-payment-preference.service'
 
 @Module({
   imports: [PrismaModule],
-  controllers: [PaymentsController],
+  controllers: [PaymentsController, SavedPaymentPreferenceController],
   providers: [
     PaymentsService,
+    SavedPaymentPreferenceService,
     {
       provide: PAYMENT_PROVIDER_TOKEN,
       useClass: DummyPaymentProvider,
     },
   ],
+  exports: [SavedPaymentPreferenceService],
 })
 export class PaymentsModule {}
